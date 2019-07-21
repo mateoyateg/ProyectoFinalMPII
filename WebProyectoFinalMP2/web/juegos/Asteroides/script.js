3cc3e125ae8e9e7b6a895f0a24b66027c200f9f3
brayanpasa99 <brayanpasa99@hotmail.com>
2019-07-21 13:48:48 -0500

    Obtención de datos y Servlet

--- WebProyectoFinalMP2/web/juegos/Asteroides/script.js
+++ WebProyectoFinalMP2/web/juegos/Asteroides/script.js
@@ -19,8 +19,8 @@ $(document).ready(inicio);
 $(document).keydown(capturaTeclado);
 
 function inicio(){
-        var nickJugador = prompt("Ingrese su nickname: ");
-        document.getElementsByName("nombreJugador").value = nickJugador;
+        var nombreJugador = prompt("Ingrese su nombre de jugador: ");
+        document.getElementById("nombreJugador").value = nombreJugador;
 	var lienzo = $("#lienzo")[0];
 	var contexto = lienzo.getContext("2d");
 	var buffer = document.createElement("canvas");
@@ -155,7 +155,7 @@ function run(){
 		contextoBuffer.font = "bold 50px sans-serif";
 		contextoBuffer.fillText("GAME OVER", 180, 200);
 		contextoBuffer.fillText(parseInt(duracion/10)+" pts", 250, 250);
-                document.getElementsByName("puntosAsteroides").value = parseInt(duracion/10);
+                document.getElementById("puntosAsteroides").value = parseInt(duracion/10);
 		contexto.clearRect(0,0,700,500);
 		contexto.drawImage(buffer, 0, 0);
 		$("button").css("display","inline");
